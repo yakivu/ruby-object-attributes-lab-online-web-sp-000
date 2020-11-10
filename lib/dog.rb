cd3cce92dba0c6e83f3a81a81916ec1c2760ee87
Adam Walker <adamw207@yahoo.com>
2020-11-10 21:00:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,6 +5,7 @@ class Dog
   def name
     @name
   end
+<<<<<<< HEAD
 
 def initialize 
    @breed = :breed 
@@ -12,10 +13,37 @@ def initialize
  def breed 
    @breed
 end
+=======
+ def name=(name) 
+  @name = name
+ end
+
+def initialize
+  @breed = :breed
+end
+def breed 
+  @breed 
+end
+
+def breed= (breed)
+  @breed = breed 
+end 
+def breed 
+  @breed
 end
 
 
+>>>>>>> f6e90dd55e5232afd36cf24985fbd4b966243287
+end
+fido = Dog.new
+fido.name
+fido.breed
 
+snoopy = Dog.new 
+snoopy.name
+
+
+<<<<<<< HEAD
 fido = Dog.new
 fido.name 
 
@@ -24,6 +52,8 @@ breed.name
 
 
 
+=======
+>>>>>>> f6e90dd55e5232afd36cf24985fbd4b966243287
     
   
 
--- lib/person.rb
+++ lib/person.rb
@@ -11,12 +11,35 @@ def initialize
  def job 
    @job
 
+<<<<<<< HEAD
   
+=======
+  def initialize
+    @name = :name
+  end
+  def name
+    @name
+  end
+
+def name=(name) 
+  @name = name
+end
+def initialize
+  @job = :job
+end
+def job 
+  @job 
+end
+
+def job=(job)
+  @job = job
+>>>>>>> f6e90dd55e5232afd36cf24985fbd4b966243287
 
 end
 end
 beyonce = Person.new
 beyonce.name
+<<<<<<< HEAD
 
  
  job = Person.new 
@@ -34,6 +57,9 @@ beyonce.name
 
 
 
+=======
+beyonce.job
+>>>>>>> f6e90dd55e5232afd36cf24985fbd4b966243287
 
 
     
