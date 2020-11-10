bcea3be4899f5da263d51506650111829798463d
Adam Walker <adamw207@yahoo.com>
2020-11-10 20:10:27 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,31 @@
-class Dog 
-def initialize(name)
-  @name = name 
-end 
-def name 
-  @name 
-end 
+class Dog
+  def initialize
+    @name = :name
+  end
+  def name
+    @name
+  end
+
+def initialize 
+   @breed = :breed 
+ end 
+ def breed 
+   @breed
+end
 end
 
 
 
+fido = Dog.new
+fido.name 
+
+breed = Dog.new 
+breed.name
 
 
 
     
+  
+
+
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,40 @@
-class Person
+class Person 
+  def initialize
+    @name = :name
+  end
+  def name
+    @name
+  end
+def initialize 
+   @job = :job
+ end 
+ def job 
+   @job
 
-end 
+  
+
+end
+end
+beyonce = Person.new
+beyonce.name
+
+ 
+ job = Person.new 
+ job.name
+
+
+ puts beyonce.job
+
+   
+   
+
+
+
+ 
+
+
+
+
+
+    
 
